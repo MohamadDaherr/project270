d3291ee95161ffb79d2cd18fafb30b2b22ba0f11
Ali-Joumaa <alijoumaa2003@gmail.com>
2024-10-18 20:26:16 +0300

    place_ships func

--- code.c
+++ code.c
@@ -2,4 +2,3 @@
 #include <stdlib.h>
 
 
-
--- functions.c
+++ functions.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+#define ship 'S'
 #define water '~'
+#define Grid_size 10
+
+#define Carrier 5
+#define Battleship 4
+#define Destroyer 3
+#define Submarine 2
+
+typedef struct {
+    char *name;
+    int size;
+} Ship;
+
+Ship ships[] = {
+    {"Carrier", Carrier},
+    {"Battleship", Battleship},
+    {"Destroyer", Destroyer},
+    {"Submarine", Submarine},
+};
+
+int total_ships = sizeof(ships) / sizeof(ships[0]);
 
 void create_grid() {
     // Initialize a 10x10 grid with '~' representing water
@@ -47,7 +69,62 @@ int difficultyLevel() {
     }
 }
 
-int main(){
-  create_grid();
-  int level = difficultyLevel();
+int check_coordinates(int row , int col, int size, char orientation, char grid[Grid_size][Grid_size]){
+    if(orientation=='H' || orientation=='h'){
+
+        //This base case to check if a ship goes ou of bounds
+        if(col+size>Grid_size)
+            return 0;
+        
+        for (int i = 0; i < size; i++) {
+            if (grid[row][col + i] != water) return 0;  
+        }
+    }
+    else if(orientation=='V' || orientation=='v'){
+        //This base case to check if a ship goes ou of bounds
+        if(row+size>Grid_size)
+            return 0;
+        
+        for (int i = 0; i < size; i++) {
+            if (grid[row+i][col] != water) return 0;  
+        }
+    }
+    return 1;
+}
+
+void place_ships(char grid[Grid_size][Grid_size]){
+    for (int i = 0; i < total_ships; i++)
+    {
+        int row,col;
+        char orientation;
+        int valid =1;
+
+        while (valid)
+        {
+            printf("Place your %s (size %d).\n");
+            printf("Enter column (A_J), row (1-10), and orientation (H for horizontal, V for vertical): ",ships[i].name,ships[i].size);
+            
+            //This variable to convert from letter to the exact index
+            char column;
+            scanf("%d %c %c", &column, &row, &orientation);          
+            col = column -'A';
+            row--;
+
+            if(check_coordinates(row,col,ships[i].size,orientation,grid)){
+                for (int i = 0; i < ships[i].size; i++)
+                {
+                    if(orientation=='H' || orientation=='h'){
+                        grid[row][col + i] = ship;
+                    }
+                    else{
+                        grid[row+i][col]= ship;
+                    }
+                }
+                valid =0;
+            }  
+            else{
+                printf("Invalid Placement. Try again\n");
+            }
+        }
+    }
 }
